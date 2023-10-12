d0f061215350ef84cb4c197bf1a233e13055524f
alekseizorin <95755507+alekseizorin@users.noreply.github.com>
2023-10-12 16:14:40 +0200

    feat: adds noop mappable, useful when a map is not required but can't be nil (#5)

---
+++ noopMappable.go
@@ -0,0 +1,11 @@
+package os_query
+
+type noopMappable struct{}
+
+func (n *noopMappable) Map() map[string]interface{} {
+	return map[string]any{}
+}
+
+func NoopMappable() *noopMappable {
+	return &noopMappable{}
+}
